bd4b6696a5af4affd276fe8882ff01c8e3267c8e
Todd Jones <anthony.jones@cgifederal.com>
2017-12-08 13:16:15 -0500

    Updated styling

--- js/views/HolidayView.js
+++ js/views/HolidayView.js
@@ -15,12 +15,6 @@ define([
 		render: function() {
 			this.$el.html('<h1>' + this.model.attributes.description + '</h1>');
 			this.$el.append(this.model.attributes.date);
-			if (this.model.attributes.image) {
-				$('body').css('background-image', "url('images/" + this.model.attributes.image + "')");
-			} else {
-				$('body').css('background-image', 'url("images/cat.jpg")');
-			}
-
 			return this;
 		}
 	});
